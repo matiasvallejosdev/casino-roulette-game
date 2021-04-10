5c7ebc105fb42c92f1ab5c69615db11622edd6a0
Matias A. Vallejos <matiasdeveloper@outlook.com>
2021-04-10 20:26:15 -0300

    fixed untracked files

---
+++ Assets/Common/Scripts/Player/PlayerRound.cs
@@ -0,0 +1,16 @@
+﻿
+[System.Serializable]
+public class PlayerRound
+{
+    public int cash;
+    public FichasSave[] fichas;
+
+    public PlayerRound(int cash,FichasSave[] fichas, bool editRound)
+    {
+        this.cash = cash;
+        if (editRound)
+        {
+            this.fichas = fichas;
+        }
+    }
+}
